db506d2e45917158c4fe76a0a8fcf615e78fb4c3
Avi Patel <avidpatel1@gmail.com>
2020-11-21 21:28:30 -0800

    added some basic navigation

--- App.js
+++ App.js
@@ -7,17 +7,42 @@ import {
   Button,
   Alert,
 } from "react-native"
-
 import Camera from "./components/Camera";
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
 
-const App = () => {
+function HomeScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+      <Button
+        title="Go to Camera"
+        onPress={() => navigation.navigate('Camera')}
+      />
+    </View>
+  );
+}
 
+function CameraScreen() {
   return (
     <View style={style.container}>
       <Camera></Camera>      
     </View>
   )
+}
+
+const Stack = createStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Camera" component={CameraScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
 
 const style = StyleSheet.create({
@@ -33,3 +58,4 @@ const style = StyleSheet.create({
 AppRegistry.registerComponent('HackGSU', () => App);
 
 export default App
+
